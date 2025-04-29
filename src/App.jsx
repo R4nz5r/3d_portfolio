2897002c82cbcda999994c945b16f4c9ee4268bb
R4nz5r <ragibshahriar43@gmail.com>
2025-04-29 19:05:54 +0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <main>
+      <h1 className="text-5xl font-extrabold">React</h1>
+     </main>
+   )
+ }
+ 
+ export default App

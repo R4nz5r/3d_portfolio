b0cb85cb59ddea83a79446ddb19cd9b36a86aabc
R4nz5r <ragibshahriar43@gmail.com>
2025-05-07 16:11:08 +0600

    Navbar section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar/>
       <Hero />
       <ShowcaseSection />
     </>

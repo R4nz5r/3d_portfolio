41babafe15b04db0bd672b81d8a0d2016a49a14a
R4nz5r <ragibshahriar43@gmail.com>
2025-05-12 22:16:21 +0600

    finished   version- 0.0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
 import Conatct from "./sections/Conatct";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
       <TechStack />
       <Testimonials />
       <Conatct />
+      <Footer />
     </>
   );
 };
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { socialImgs } from '../constants'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <div className="footer-container">
+            <div className="flex flex-col justify-center md:items-start items-center">
+                <a href="/">Visit my blog</a>
+            </div>
+            <div className="socials">
+                {socialImgs.map((img) => (
+                    <a className='icon' href={img.url} target='_blank' key={img.url}>
+                        <img src={img.imgPath} alt="" />
+                    </a>
+                ))}
+            </div>
+            <div className="flex flex-col justify-center">
+                <p className="text-center md:text-end">
+                    ©️{new Date().getFullYear()} Ragib . All rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
